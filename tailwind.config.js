8e7233ef8d2afec3c157de026ad44362210b64d1
Aryan Pandey <notlelouchvb@gmail.com>
2023-02-25 21:55:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: '#F25F3A',
+        brightRedLight: '#f6866a',
+        brightRedSupLight: '#fdece7',
+        darkBlue: '#242d52',
+        darkGrayishBlue: '#24262e',
+        veryDarkBlue: '#1d1e25',
+        veryPaleRed: '#ffefeb',
+        veryLightGray: '#fafafa',
+      },
+    },
+  },
+  plugins: [],
+}
